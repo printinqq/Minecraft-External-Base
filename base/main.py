57d01738e4789d84bee6659d6a8fab361d70cfe8
printinqq <118393008+printinqq@users.noreply.github.com>
2023-09-09 05:58:13 +0100

    Update main.py

--- base/main.py
+++ base/main.py
@@ -1,20 +1,23 @@
 # Modules
 import psutil
 import time
+import os
 
-# Checks if Javaw.exe is running (Minecraft.)
+# Makes Console Look Cleaner.
+os.system("cls")
+
 def check_javaw_running():
     while True:
         for proc in psutil.process_iter(['name']):
             if proc.info['name'] == 'javaw.exe':
-                run_autoclicker()  # Call your function here.
+                run_autoclicker()  # Call your autoclicker function here.
                 return True
         else:
             return False
         time.sleep(1)
 
 def run_autoclicker():
-    # Your autoclicker code goes here, or whatever you decide to add.
+    # Your autoclicker code goes here, or whatever you make.
     # Replace the following print statement with your actual autoclicker logic.
     print("Minecraft External Base")
 
@@ -23,4 +26,4 @@ if check_javaw_running():
 else:
     print("No Java Process is found. Please make sure Minecraft is running.")
 
-input("") # Remove this line once you have added your autoclicker code, or whatever your adding.
+input("") # remove this once you have added your autoclicker code, or whatever your adding.
